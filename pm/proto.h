1461e84da50a62a971a330e853fc8156fa193ac6
Cristian Canales <donwea@MacBook-Air-de-Cristian.local>
2015-10-31 00:35:26 -0300

    trheaddfasdfasd

--- pm/proto.h
+++ pm/proto.h
@@ -50,7 +50,7 @@ int do_getrusage(void);
 int do_mycall(void);
 int do_mutex_init(void);
 int do_mutex_destroy(void);
-int do_mutex_lock(const kmutex_t *mtx);
+int do_mutex_lock(kmutex_t *mtx);
 int do_mutex_unlock(void);
 
 /* schedule.c */
